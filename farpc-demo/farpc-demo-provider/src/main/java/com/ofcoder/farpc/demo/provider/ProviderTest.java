aacdebebde056ce98991b7ea8b429cc8c67882af
tynan.liu <572204740@qq.com>
2019-06-25 09:27:15 +0800

    log

--- farpc-demo/farpc-demo-provider/src/main/java/com/ofcoder/farpc/demo/provider/ProviderTest.java
+++ farpc-demo/farpc-demo-provider/src/main/java/com/ofcoder/farpc/demo/provider/ProviderTest.java
@@ -4,6 +4,8 @@ import com.ofcoder.farpc.demo.api.IWelcome;
 import com.ofcoder.farpc.registry.IRegistrar;
 import com.ofcoder.farpc.registry.zookeeper.ZookeeperRegistrarImpl;
 import com.ofcoder.farpc.rpc.netty.RpcServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -11,7 +13,10 @@ import java.io.IOException;
  * @author far.liu
  */
 public class ProviderTest {
+    private static final Logger logger = LoggerFactory.getLogger(ProviderTest.class);
+
     public static void main(String[] args) throws IOException {
+        logger.info("hello guys.");
         IWelcome welcome = new WelcomeImpl();
         IRegistrar registrar = new ZookeeperRegistrarImpl();
 
--- farpc-registry/src/main/java/com/ofcoder/farpc/registry/zookeeper/ZookeeperConfig.java
+++ farpc-registry/src/main/java/com/ofcoder/farpc/registry/zookeeper/ZookeeperConfig.java
@@ -4,7 +4,7 @@ package com.ofcoder.farpc.registry.zookeeper;
  * @author far.liu
  */
 public class ZookeeperConfig {
-    public static final String ADDRESS = "127.0.0.1:2181";
+    public static final String ADDRESS = "192.168.1.118:2181";
     public static final String FOLDER = "/faregistrys";
 
 }
