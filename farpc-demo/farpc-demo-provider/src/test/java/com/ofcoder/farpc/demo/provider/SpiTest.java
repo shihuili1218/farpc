cc50962fc78cc1ab21a4aa577aaf869727a5f58e
tynan.liu <572204740@qq.com>
2019-06-25 15:47:58 +0800

    spi fix

--- farpc-demo/farpc-demo-provider/src/test/java/com/ofcoder/farpc/demo/provider/SpiTest.java
+++ farpc-demo/farpc-demo-provider/src/test/java/com/ofcoder/farpc/demo/provider/SpiTest.java
@@ -1,5 +1,10 @@
 package com.ofcoder.farpc.demo.provider;
 
+import com.ofcoder.farpc.cluster.ILoadbalance;
+import com.ofcoder.farpc.cluster.LoadbalanceFactory;
+import com.ofcoder.farpc.common.loader.ExtensionLoader;
+import com.ofcoder.farpc.registry.IRegistrar;
+import com.ofcoder.farpc.registry.RegistrarFactory;
 import com.ofcoder.farpc.rpc.netty.RpcServer;
 import org.junit.Test;
 
@@ -11,11 +16,20 @@ import java.io.IOException;
  */
 public class SpiTest {
     @Test
-    public void test() throws IOException {
+    public void start() throws IOException {
         RpcServer server = new RpcServer();
         server.init("127.0.0.1:20880");
         server.bind("com.ofcoder.farpc.demo.api.IWelcome",  new WelcomeImpl());
         server.publisher();
         System.in.read();
     }
+
+
+    @Test
+    public void test(){
+        ILoadbalance loadbalance = LoadbalanceFactory.getLoadbalance();
+        IRegistrar registrar = RegistrarFactory.getRegistrar();
+        System.out.println();
+    }
+
 }
