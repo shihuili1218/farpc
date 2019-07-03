5eda9369922a71e501ca865db2533c76b354281b
yzker <gege87417376@qq.com>
2019-07-03 22:46:38 +0800

    refactor

--- farpc-registry/src/main/java/com/ofcoder/farpc/registry/IRegistrar.java
+++ farpc-registry/src/main/java/com/ofcoder/farpc/registry/IRegistrar.java
@@ -8,8 +8,17 @@ import com.ofcoder.farpc.common.anno.FarSPI;
 @FarSPI("zookeeper")
 public interface IRegistrar {
 
+    /**
+     * 初始化
+     * @param registerAddress zookeeper地址，例如127.0.0.1:2181
+     */
     void init(String registerAddress);
 
+    /**
+     * 注册服务
+     * @param providerAddress 服务提供者地址
+     * @param service 服务
+     */
     void register(String providerAddress, String service);
 
     String discover(String service);
